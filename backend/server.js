5560d5c13f8485e647b93850415127dcfdd56116
Daniel Obede <danielobede.devdesigner@gmail.com>
2026-09-01 22:37:39 -0300

    fix: adaptar porta do backend para deploy

--- backend/server.js
+++ backend/server.js
@@ -604,8 +604,8 @@ server.delete(
 
 server.use(router);
 
-server.listen(3001, () => {
-  console.log(
-    "🚀 JSON Server rodando em http://localhost:3001"
-  );
+const PORT = process.env.PORT || 3001
+
+server.listen(PORT, () => {
+  console.log(`🚀 JSON Server rodando na porta ${PORT}`)
 });
